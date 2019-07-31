980b204b86074259cba74d331bec90c638fce665
unknown <math.moura@hotmail.com>
2019-07-31 12:42:26 -0300

    Direcao da Nave Funcionando

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Game:
 		self.running = True
 		self.bg = pygame.image.load('background.jpg')
 		self.bg = smoothscale(self.bg,(int(WIDTH),int(HEIGHT)))
-		self.sps = NAVE(100,100)
+		self.sps = NAVE(400,400)
 		self.inimigo = [] 
 		self.start_time = time.time()
 		self.temp_antigo = -1
@@ -42,6 +42,7 @@ class Game:
 		end_time = time.time()
 		temp_novo = int(end_time) - int(self.start_time)
 		#print(temp_novo)
+		'''
 		if(temp_novo != self.temp_antigo):
 			#print(temp_novo)
 			self.temp_antigo = temp_novo
@@ -60,19 +61,28 @@ class Game:
 				ang = randint(0,180)
 				vel = randint(10,100)/10
 				self.inimigo.append(INIMIGO(posx,posy,lado,ang,vel))
-		print(len(self.inimigo))
-		self.sps.update()
+		#print(len(self.inimigo))
+		
 		for el in self.inimigo:
 			el.update()
 			if(el.x <= 0-200 or el.x >= WIDTH+200 or el.y >=HEIGHT+200 or el.y <=0-200):
 				self.inimigo.remove(el)
+		'''
+		self.sps.update()
 
 	def draw(self):
 		# Game loop - Draw
 		self.screen.blit(self.bg,(0,0))
 		self.sps.draw(self.screen)
+		for i in range(20):
+			pygame.draw.line(self.screen,(255,0,0),(i*50,0),(i*50,HEIGHT),1)
+		for i in range(20):
+			pygame.draw.line(self.screen,(255,0,0),(0,i*50),(WIDTH,i*50),1)
+		
+		'''
 		for el in self.inimigo:
 			el.draw(self.screen)
+		'''
 		# after drawing everything, flip the display
 		pygame.display.flip()
 
--- nave.py
+++ nave.py
@@ -11,55 +11,61 @@ class NAVE:
         self.vel = 5
         self.balas=[]
         self.nave = pygame.image.load('spaceship.png')
-        self.sub = self.nave.subsurface(40,0,40,30)
-        self.ang = 0
-        self.ad_ang = 0.1
-        self.a = 0
+        self.sub = self.nave.subsurface(39,0,39,40)
+        self.sub = smoothscale(self.sub,(int(39*4), int(160)))
         self.rot = 0  
         self.rot_speed = 0 
+
         # define a surface (RECTANGLE)  
-        self.image_orig = self.nave.subsurface(40,0,40,30)
-        # for making transparent background while rotating an image  
-        #self.image_orig.set_colorkey(BLACK)  
-        # fill the rectangle / surface with green color  
-        self.image = self.image_orig.copy()  
-        #self.image.set_colorkey(BLACK)  
+        self.image_orig = self.sub
+        self.image = self.image_orig.copy()   
         # define rect for placing the rectangle at the desired position  
         self.rect = self.image.get_rect()  
-        self.rect.center = (WIDTH // 2 , HEIGHT // 2) 
+        self.rect.center = (self.x , self.y) 
 
+        self.pontos_verdes=[]
 
     def draw(self,screen):
         for el in self.balas:
             el.draw(screen)
-
-        # making a copy of the old center of the rectangle  
-        old_center = self.rect.center  
-        # defining angle of the self.rotation  
-        self.rot = (self.rot + self.rot_speed) % 360  
-        #print(self.rot)
-        # self.rotating the orignal image  
-        new_image = pygame.transform.rotate(self.image_orig , self.rot)  
-        self.rect = new_image.get_rect()  
-        # set the rotated rectangle to the old center  
-        self.rect.center = old_center  
-        # drawing the rotated rectangle to the screen  
-        #print(self.rect)
-        screen.blit(new_image , self.rect)#(self.x,self.y,self.rect[2],self.rect[3]))  #
-        # flipping the display after drawing everything  
+        #pygame.draw.circle(screen, (0,255,0),(450,450),50,1)
         
-        
+        screen.blit(self.new_image , self.rect)
+        for el in self.pontos_verdes:
+            pygame.draw.circle(screen, (0,255,0),el,10)
 
     def update(self):
         for el in self.balas:
             el.update()
             if(el.x <= 0 or el.x >= WIDTH or el.y >=HEIGHT or el.y <=0):
                 self.balas.remove(el)
-
+        # making a copy of the old center of the rectangle  
+        old_center = self.rect.center  
+        # defining angle of the self.rotation  
+        self.rot = (self.rot + self.rot_speed) % 360  
+        # self.rotating the orignal image  
+        self.new_image = pygame.transform.rotate(self.image_orig , self.rot)  
+        self.rect = self.new_image.get_rect()  
+        # set the rotated rectangle to the old center  
+        self.rect.center = old_center  
+        # drawing the rotated rectangle to the screen 
+        
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            self.rect[1] -= self.vel*np.cos((np.pi*self.rot)/180)
-            self.rect[0] -= self.vel*np.sin((np.pi*self.rot)/180)
+            #self.rect[0] -= self.vel*np.sin(ang_rad)
+            #self.rect[1] -= self.vel*np.cos(ang_rad)
+            #print("X: {} Y: {} Rot: {} Rot_speed: {} Angulo: {}".format(self.rect[0],self.rect[1],self.rot,self.rot_speed,ang_rad))
+
+            ang_rad = (np.pi*self.rot)/180
+
+            self.x -= self.vel*np.sin(ang_rad)
+            self.y -= self.vel*np.cos(ang_rad)
+
+            self.rect.center = self.x, self.y
+            self.pontos_verdes.append(self.rect.center)
+
+            print("Rect: {} Center {} Ang: {} Sin: {:.2f} Cos: {:.2f}".format(self.rect,self.rect.center,self.rot,np.sin(ang_rad),np.cos(ang_rad)))
+           
         if keys[pygame.K_LEFT]:
             self.rot_speed=5
         elif keys[pygame.K_RIGHT]:
@@ -67,4 +73,6 @@ class NAVE:
         else:
             self.rot_speed = 0
         if keys[pygame.K_SPACE]:
-            self.balas.append(BALA(self.rect[0],self.rect[1],(np.pi*self.rot)/180))
+            self.balas.append(BALA(self.rect[0],self.rect[1],ang_rad))
+        
+        
